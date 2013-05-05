90dec9df674423924f357250ba727c5e67e69d22
John Liu <elite_jigang@163.com>
2013-05-05 11:59:42 +0800

    level 4

---
+++ magic/python/riddle4.py
@@ -0,0 +1,22 @@
+__author__ = 'JohnLiu'
+
+import urllib2
+import re
+
+req = urllib2.Request('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345')
+response = urllib2.urlopen(req)
+the_page = response.read()
+number = re.findall('(\d+)', the_page)
+while True:
+    req = urllib2.Request('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s'% number)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+    print the_page
+    match_list =  re.findall('(\d+)', the_page)
+    if match_list:
+        number = match_list[0]
+        print number
+    else:
+        break
+
+#get the answer '***.html'

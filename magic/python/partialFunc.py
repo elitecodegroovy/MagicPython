a55d4cfbae2947bb238d390e9782da9078d03a96
elitecodegroovy <elite_jigang@163.com>
2013-10-04 10:06:58 +0800

    tuples

--- magic/python/partialFunc.py
+++ magic/python/partialFunc.py
@@ -3,8 +3,7 @@ from functools import partial
 import Tkinter
 
 root = Tkinter.Tk()
-MyButton = partial(Tkinter.Button, root,
-    fg='white', bg='blue')
+MyButton = partial(Tkinter.Button, root, fg='white', bg='blue')
 b1 = MyButton(text='Button 1')
 b2 = MyButton(text='Button 2')
 qb = MyButton(text='QUIT', bg='red',
--- magic/python/readTextFile.py
+++ magic/python/readTextFile.py
@@ -1,11 +1,34 @@
 __author__ = 'JohnLiu'
-fileName = 'pythonMakeAFile.txt'
+import os
+
+fileName = 'readTextFile.py'
 try:
-    fobj = open(fileName, 'r')
+    lines = open(fileName, 'r')
+    data = [line.strip() for line in lines.readlines()]
+    for data in data:
+        print data
 except IOError, e:
     print "***file open error", e
-else:
-    #print the file content
-    for eachLine in fobj:
-        print eachLine
-    fobj.close()
+# else:
+#     for eachLine in lines:
+#         print eachLine
+#     lines.close()
+for tmpdir in ('/TEMP', r'c:\TEMP'):
+    if os.path.isdir(tmpdir):
+        break
+    else:
+        print 'No directory is available!'
+
+if tmpdir:
+    os.chdir(tmpdir)
+    cwd = os.getcwd()
+    print '*** current temporary directory'
+    print cwd
+    print '*** creating example directory...'
+    os.mkdir('example')
+    os.chdir('example')
+    cwd = os.getcwd()
+    print '*** new working directory:'
+    print cwd
+    print '*** original directory listing:'
+    print os.listdir(cwd)

97eba4403a4fabbd02a620a481e48a2af863e5e1
elitecodegroovy <elite_jigang@163.com>
2013-10-07 08:30:02 +0800

    Add a proxy config if it failed to connect the internet directly.

--- magic/python/urlUtil.py
+++ magic/python/urlUtil.py
@@ -3,10 +3,18 @@ __author__ = 'JohnLiu'
 import urllib2
 import re
 
+#------------------------------------------------------------
+#Add the proxy if you failed to connect the internet directly.
+#------------------------------------------------------------
+# proxy_support = urllib2.ProxyHandler({"http":"http://127.0.0.1:8087"})
+# opener = urllib2.build_opener(proxy_support)
+# urllib2.install_opener(opener)
+
 req = urllib2.Request('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345')
 response = urllib2.urlopen(req)
 the_page = response.read()
 number = re.findall('(\d+)', the_page)
+print number
 while True:
     req = urllib2.Request('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s'% number)
     response = urllib2.urlopen(req)
